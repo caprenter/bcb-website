bbf32d8f09fcffb558acf8a16631bcc95c3c9d95
shi <notshi@gmail.com>
2014-06-17 17:47:59 +0100

    Update archive-presenter.php
    Added new class for widget-wrappers for presenter profiles.

--- archive-presenter.php
+++ archive-presenter.php
@@ -1,4 +1,7 @@
 <?php
+/*
+Template Name: Presenters Page
+*/
 /*Created by caprenter for BCB*/
 // Exit if accessed directly
 if( !defined( 'ABSPATH' ) ) {
@@ -6,47 +9,71 @@ if( !defined( 'ABSPATH' ) ) {
 }
 
 /**
- * Archive Presenter Template
+ * Presenters Template
  *
  *
- * @file           archive-presenter.php
+ * @file           single-presenter.php
  * @package        Responsive
  * @author         Emil Uzelac
  * @copyright      2003 - 2013 ThemeID
  * @license        license.txt
- * @version        Release: 1.1
- * @filesource     wp-content/themes/responsive/archive.php
- * @link           http://codex.wordpress.org/Theme_Development#Archive_.28archive.php.29
+ * @version        Release: 1.0
+ * @filesource     wp-content/themes/responsive/page.php
+ * @link           http://codex.wordpress.org/Theme_Development#Pages_.28page.php.29
  * @since          available since Release 1.0
  */
 
 get_header(); ?>
 
-<div id="content-archive" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
+<a href="http://www.bcbradio.co.uk/presenters/" class="back_presenters">Back to Presenters</a>
 
-<h1 class="present">Radio Presenters</h1>
+<div id="content" class="<?php echo implode( ' ', responsive_get_content_classes() ); ?>">
 
 	<?php if( have_posts() ) : ?>
 
-		<?php get_template_part( 'loop-header' ); ?>
-    
-    <?php query_posts($query_string . '&orderby=title&order=ASC'); ?>
 		<?php while( have_posts() ) : the_post(); ?>
-    
+
+			<?php get_template_part( 'loop-header' ); ?>
 
 			<?php responsive_entry_before(); ?>
 			<div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-				<?php responsive_entry_top(); ?>
+        
+        <?php responsive_entry_top(); ?>
 
-				<?php get_template_part( 'post-meta' ); ?>
+				<?php get_template_part( 'post-meta-page' ); ?>
+        <?php if( has_post_thumbnail() ) : ?>
+          <?php the_post_thumbnail( 'large', array( 'class' => 'presenter-image' ) ); ?>
+        <?php endif; ?>
 
 				<div class="post-entry">
-					<?php if( has_post_thumbnail() ) : ?>
-						<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>">
-							<?php the_post_thumbnail( 'thumbnail', array( 'class' => 'alignleft' ) ); ?>
-						</a>
-					<?php endif; ?>
-					<?php the_excerpt(); ?>
+          <?php //echo print_r(get_post_custom()); ?>
+          <div class="presenter-content">
+					<?php the_content( __( 'Read more &#8250;', 'responsive' ) ); ?>
+          </div>
+          <!--Custom Field Data-->
+          <div class="widget-wrapper presenter">
+            <h3 style="margin-top:0">Programmes</h3>
+            <p><?php echo get_post_meta($post->ID, "programmes", true); ?></p>
+              <?php 
+                $email = get_post_meta($post->ID, "email", true);
+                $twitter = get_post_meta($post->ID, "twitter-name", true);
+                $facebook = get_post_meta($post->ID, "facebook-name", true);
+                if ($email || $twitter || $facebook ) {
+                  echo '<h3 class="presenter-contact">Contact</h3>';
+                  if ($email) {
+                    echo "Email: " . htmlentities($email) . "<br/>"; 
+                  }
+                  if ($twitter) {
+                    echo 'Twitter: <a href="https://twitter.com/' . htmlentities($twitter) . '">' . htmlentities($twitter) . '</a><br/>';  
+                  }                  
+                  if ($facebook) {
+                    echo 'Facebook: <a href="https://www.facebook.com/' . htmlentities($facebook) . '">"' . htmlentities($facebook) . '</a><br/>'; 
+                  }
+                }
+              ?>
+
+          </div>
+          <?php //echo get_post_meta($post->ID, "surname", true); ?>
 					<?php wp_link_pages( array( 'before' => '<div class="pagination">' . __( 'Pages:', 'responsive' ), 'after' => '</div>' ) ); ?>
 				</div>
 				<!-- end of .post-entry -->
@@ -56,6 +83,10 @@ get_header(); ?>
 				<?php responsive_entry_bottom(); ?>
 			</div><!-- end of #post-<?php the_ID(); ?> -->
 			<?php responsive_entry_after(); ?>
+
+			<?php responsive_comments_before(); ?>
+			<?php comments_template( '', true ); ?>
+			<?php responsive_comments_after(); ?>
 
 		<?php
 		endwhile;
@@ -69,7 +100,7 @@ get_header(); ?>
 	endif;
 	?>
 
-</div><!-- end of #content-archive -->
+</div><!-- end of #content -->
 
 <?php get_sidebar(); ?>
 <?php get_footer(); ?>
